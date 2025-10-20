6587b57cea75ce2d7c39b098d720ef8f6aa7ef2c
Xavier Genereux <xaviergenereux@hotmail.com>
2025-10-20 13:54:25 +0200

    remove Basic.lean

--- FormalizationInLean2025.lean
+++ FormalizationInLean2025.lean
@@ -1,4 +1,3 @@
 -- This module serves as the root of the `FormalizationInLean2025` library.
 -- Import modules here that should be built as part of the library.
-import «FormalizationInLean2025».Basic
 import «FormalizationInLean2025».FiL_Homework1_Logic
