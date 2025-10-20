497562f629e6fd3ff86b0eea19b785d4a1c112be
Xavier Genereux <xaviergenereux@hotmail.com>
2025-10-20 13:53:50 +0200

    add homework1

--- FormalizationInLean2025.lean
+++ FormalizationInLean2025.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `FormalizationInLean2025` library.
 -- Import modules here that should be built as part of the library.
 import «FormalizationInLean2025».Basic
+import «FormalizationInLean2025».FiL_Homework1_Logic
